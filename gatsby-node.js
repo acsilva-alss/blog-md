7642d5034de0a3825b97714dc64411eeecbe6a8c
Alisson Chagas Silva <alisson.cs@hotmail.com>
2021-06-08 07:52:13 -0300

    Create template of blog

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+ const path = require('path')
+ exports.createPages = async({graphql, actions}) => {
+    const { createPage } = actions
+    const posts = await graphql(`
+        query {
+            posts: allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            description
+                            path
+                            title
+                        }
+                    }
+                }
+            }
+        }  
+    `)
+    posts.data.posts.edges.forEach(post => {
+        const template = path.resolve('src/templates/post.js')
+        createPage({
+            path: post.node.frontmatter.path,
+            component: template,
+            context: {
+                id: post.node.frontmatter.path
+            }
+        })
+    });
+ }
---
+++ src/templates/post.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {graphql} from 'gatsby' 
+import Seo from '../components/Seo'
+const Post = ({data}) => {
+    return (
+        <div>
+            <Seo title={data.markdownRemark.frontmatter.title} description={data.markdownRemark.frontmatter.description}/>
+            <h1>{data.markdownRemark.frontmatter.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}/>
+        </div>
+    )
+}
+
+export const pageQuery = graphql`
+    query($id: String!){
+        markdownRemark(frontmatter: { path: {eq: $id}}) {
+            frontmatter{
+                title
+                description
+            }
+            html
+        }
+    }
+`
+
+export default Post
